668df67762ffc8876764c3dc59c4eabb74619746
hidarfaqeeh <hidarfaqeeh@gmail.com>
2026-02-20 05:00:45 +0000

    Keep visual transitions enabled while preserving pre-paint theme/lang init

--- socialpanel_ui/src/main.tsx
+++ socialpanel_ui/src/main.tsx
@@ -3,14 +3,5 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  const rootHtml = document.documentElement;
-  if (rootHtml.classList.contains("preload")) {
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        rootHtml.classList.remove("preload");
-      });
-    });
-  }
-
   createRoot(document.getElementById("root")!).render(<App />);
   
